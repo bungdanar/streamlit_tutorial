2ce5e95701cdafa86c40a7374b63668da4d7ee68
Danar Putera <danar.g.putera@gmail.com>
2025-09-03 13:53:24 +0700

    Finish: Session State

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ main_page = st.Page("pages/main_page.py", title="Main Page", icon="🎈")
 page_2 = st.Page("pages/page_2.py", title="Page 2", icon="❄️")
 page_3 = st.Page("pages/page_3.py", title="Page 3", icon="🎉")
 uber_page = st.Page('pages/uber_pickups.py')
+counter_page = st.Page('pages/counter.py')
 
 # Set up navigation
-pg = st.navigation([main_page, page_2, page_3, uber_page])
+pg = st.navigation([main_page, page_2, page_3, uber_page, counter_page])
 
 # Run the selected page
 pg.run()
