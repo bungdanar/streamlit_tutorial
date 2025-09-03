e07ae5f820a84123c0977f0411420cb361dcbbf0
Danar Putera <danar.g.putera@gmail.com>
2025-09-03 15:01:03 +0700

    Finish: Forms

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ page_2 = st.Page("pages/page_2.py", title="Page 2", icon="❄️")
 page_3 = st.Page("pages/page_3.py", title="Page 3", icon="🎉")
 uber_page = st.Page('pages/uber_pickups.py')
 counter_page = st.Page('pages/counter.py')
+forms_page = st.Page('pages/forms.py')
 
 # Set up navigation
-pg = st.navigation([main_page, page_2, page_3, uber_page, counter_page])
+pg = st.navigation(
+    [main_page, page_2, page_3, uber_page, counter_page, forms_page])
 
 # Run the selected page
 pg.run()
